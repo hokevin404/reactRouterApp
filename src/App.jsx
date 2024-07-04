e5c1bf863ff2d2bff486725f53fa0c61f7d59bdc
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 22:39:25 -0700

    Import Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+// Import pages
 import Main from './pages/Main';
 import Price from './pages/Price';
 import Currencies from './pages/Currencies';
+// Import components
+import Nav from './components/Nav';
+// Import modules
 import {Router, Routes} from 'react-router-dom';
 import { useState } from 'react';
 import './App.css'
@@ -9,6 +13,7 @@ function App() {
 
   return (
     <div className='App'>
+      <Nav />
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/price' element={<Price />} />
