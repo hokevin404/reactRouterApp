fef69d3b0a83a05deaad98da14101ba7f4d663cd
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 23:19:45 -0700

    Created loaded()

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -28,9 +28,24 @@ function Price() {
         }
     };
 
+    // useEffect to run getCoin when component mounts
     useEffect(() => {
         getCoin();
     }, []);
+
+    // loaded function for when data is fetched 
+    function loaded() {
+        return (
+            <div>
+                <h1>
+                    {coin.asset_id_base} / {coin.asset_id_quote}
+                </h1>
+                <h2>
+                    {coin.rate}
+                </h2>
+            </div>
+        );
+    };
 
     return (
         <h1>
