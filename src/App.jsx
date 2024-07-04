457f4964b8088931ee65f60798e7bc7ff987fbb4
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 22:31:38 -0700

    Added imports and routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import Main from './pages/Main';
+import Price from './pages/Price';
+import Currencies from './pages/Currencies';
+import {Router, Routes} from 'react-router-dom';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/price' element={<Price />} />
+        <Route path='/currencies' element={<Currencies />} />
+      </Routes>
+    </div>
   )
 }
 
