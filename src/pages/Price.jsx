80cd3425d8c4bdb1189517308bb87a422a4303ec
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 23:13:45 -0700

    Created coin URL

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Price() {
-  return (
-    <h1>
-        This is the Price Component.
-    </h1>
-  )
+
+    // API Key
+    const apiKey = "DBB7F384-EA45-4F83-A4B91C4BDD3F5559";
+
+    // Grab currency symbol from URL param
+    const params = useParams();
+    const symbol = params.symbol;
+
+    // Using 'params' and symbol' variable to create URL
+    const url = `http://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+
+    return (
+        <h1>
+            This is the Price Component.
+        </h1>
+    )
 }
 
 export default Price
