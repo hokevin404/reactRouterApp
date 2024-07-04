524980467426b7dd922366a9b9cbc190855c4774
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 22:48:27 -0700

    Built Currencies page

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path='/' element={<Main />} />
-        <Route path='/price' element={<Price />} />
+        <Route path='/price/:symbol' element={<Price />} />
         <Route path='/currencies' element={<Currencies />} />
       </Routes>
     </div>
