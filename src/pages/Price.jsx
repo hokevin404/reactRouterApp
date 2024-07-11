de854edf1f340ef22cb8afa347b412559dee69e5
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-11 14:59:01 -0700

    Refactor

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -3,15 +3,12 @@ import { useParams } from "react-router-dom";
 
 function Price() {
 
-    // API Key
-    const apiKey = `DBB7F384-EA45-4F83-A4B9-1C4BDD3F5559`;
-
     // Grab currency symbol from URL param
     const params = useParams();
     const symbol = params.symbol;
 
     // Using 'params' and symbol' variable to create URL
-    const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+    const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${import.meta.env.VITE_apiKey}`;
     
     // State to hold coin data
     const [coin, setCoin] = useState('null');
